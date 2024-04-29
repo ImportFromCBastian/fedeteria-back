21aca28cfd34ddd638af8c5fdb9c9ba2712f7e7c
ImportFromCBastian <sebshndz2001@gmail.com>
2024-04-28 21:11:41 -0300

    Added Husky pre-commit hook to run lint-staged

--- src/controller/mailing.controller.js
+++ src/controller/mailing.controller.js
@@ -1,12 +1,12 @@
-import { MailingModel } from '../model/mailing.model.js';
+import { MailingModel } from '../model/mailing.model.js'
 
 export class MailingController {
-	static async sendChangePasswordEmail(req, res) {
-		const { mailBody } = req.body;
-		const objectMail = await MailingModel.sendChangePasswordEmail(mailBody);
-		//send status code 201 for created mail
-		res.status(201).json(objectMail);
-	}
+  static async sendChangePasswordEmail(req, res) {
+    const { mailBody } = req.body
+    const objectMail = await MailingModel.sendChangePasswordEmail(mailBody)
+    //send status code 201 for created mail
+    res.status(201).json(objectMail)
+  }
 }
 
-export default MailingController;
+export default MailingController
