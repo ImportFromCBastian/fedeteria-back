838dfede6a53085171f9351c19d1699a84b16f63
Ignacio <meloignacionicolas@gmail.com>
2024-05-29 22:07:01 -0300

    feat: Agregar funcionalidad de eliminación de sucursal

--- src/model/Sucursal/sucursal.js
+++ src/model/Sucursal/sucursal.js
@@ -11,4 +11,17 @@ export class SucursalModel {
     const result = await connection.query(querySucursal, [sucursal.nombre, sucursal.calle, sucursal.numero, sucursal.piso, sucursal.depto])
     return result
   }
+  static async delete(id) {
+    const queryCliente = `SELECT DNI FROM Cliente WHERE idLocal = ?;`
+    const [resultCliente] = await connection.query(queryCliente, [id])
+
+    // Verifica si hay clientes asociados
+    if (resultCliente.length > 0) {
+      throw new Error('No se puede eliminar la sucursal porque tiene datos asociados.')
+    }
+    // Si no hay clientes, procede con la eliminación de la sucursal
+    const querySucursal = `DELETE FROM Local WHERE idLocal = ?;`
+    const result = await connection.query(querySucursal, [id])
+    return result
+  }
 }
