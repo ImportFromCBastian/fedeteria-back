9421d29bc7331eb1d1061305f24561444b2a9439
MaxPower0345 <fabrizioperri03@gmail.com>
2024-06-01 01:24:53 -0300

    avance

--- src/model/publication/Publication.js
+++ src/model/publication/Publication.js
@@ -13,7 +13,18 @@ export class PublicationModel {
     return result.insertId // Devuelve el ID de la publicación creada
   }
   static async findPublicationById(idPublication) {
-    const query = `SELECT * FROM Publication WHERE idPublication = ?;`
+    const query = `SELECT * FROM Publicacion WHERE idPublicacion = ?;`
     return await connection.query(query, [idPublication]).catch(error => new Error(error))
+  }
+  static async update(idPublication, publication) {
+    const query = `UPDATE Publicacion SET nombre = ?, descripcion = ?,precio = ?, productoACambio = ?, estado = ? WHERE idPublicacion = ?;`
+    return await connection.query(query, [
+      publication.nombre,
+      publication.descripcion,
+      publication.precio,
+      publication.producto_a_cambio,
+      publication.estado,
+      idPublication
+    ])
   }
 }
--- src/model/publication/schema/PublicationSchema.js
+++ src/model/publication/schema/PublicationSchema.js
@@ -1,7 +1,5 @@
 import { z } from 'zod'
 const publicationSchema = z.object({
-  //el dni va a venir ya del publicante no hay que chequearlo
-  dni: z.number({ invalid_type_error: 'El dni debe ser un numero' }).positive(),
   //nombre validations
   nombre: z.string({
     required_error: 'El nombre de la publicacion es requerido'
@@ -18,9 +16,27 @@ const publicationSchema = z.object({
   })
 })
 
+const partialPublicationSchema = z.object({
+  //nombre validations
+  nombre: z.string({
+    required_error: 'El nombre de la publicacion es requerido'
+  }),
+  //descripcion validations
+  descripcion: z.string({
+    required_error: 'Se requiere la descripcion'
+  }),
+  precio: z.number().min(0).optional(),
+  //producto a cambio validations
+  producto_a_cambio: z.string(),
+  //estado validations
+  estado: z.string({
+    required_error: 'Se requiere el estado de la publicacion'
+  })
+})
+
 export const publicationValidator = publication => {
   return publicationSchema.safeParse(publication)
 }
 export const partialPublicationValidator = publication => {
-  return publicationSchema.partial().safeParse(publication)
+  return partialPublicationSchema.partial().safeParse(publication)
 }
