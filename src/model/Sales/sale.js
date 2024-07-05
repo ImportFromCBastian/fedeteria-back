2d64574d1e5f6ba6cb0d7d4f5be973163bda27d4
Sebastian Hernandez <sebshndz2001@gmail.com>
2024-07-05 16:03:49 -0300

    feat: Add fecha column to Venta table
    The code changes include adding a new column `fecha` to the `Venta` table in the database. This column will store the date of the sale transaction.
    
    This commit message follows the established convention of using a prefix (`feat`) to indicate the type of change and provides a clear and concise description of the added functionality.

--- src/model/Sales/sale.js
+++ src/model/Sales/sale.js
@@ -2,8 +2,8 @@ import connection from '../../settings/database.js '
 export class SaleModel {
   static async create(paymentMethod, clientDNI, workerDNI, precio, idLocal) {
     const query = `
-      INSERT INTO Venta (pago, dniCliente, dniEmpleado,precioTotal, idLocal)
-      VALUES (?,?,?,?,?)
+      INSERT INTO Venta (pago, dniCliente, dniEmpleado,precioTotal, idLocal,fecha)
+      VALUES (?,?,?,?,?,NOW())
     `
     try {
       const [result] = await connection.query(query, [paymentMethod, clientDNI, workerDNI, precio, idLocal])
